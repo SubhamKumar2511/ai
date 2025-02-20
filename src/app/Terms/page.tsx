0edcb0fefe6ffce5b34d391d35b6f7ac511cd91a
Subham Kumar <subhamkumar98998@gmail.com>
2025-02-21 04:59:38 +0530

    hro

---
+++ src/app/Terms/page.tsx
@@ -0,0 +1,12 @@
+import TermsAndConditions from '@/components/terms'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+          <TermsAndConditions/>
+    </div>
+  )
+}
+
+export default page
